a3547524c4c74e772ee1339426accefb55870bf1
Bonus Mom <AZonaGarcia@aol.com>
2020-08-18 14:13:12 -0400

    added working carousel

--- assets/script.js
+++ assets/script.js
@@ -54,3 +54,37 @@ $("#ingredients").on("click", function (event) {
         console.log(response);
     });
 });
+
+$(document).ready(function () {
+
+
+    $('.center').slick({
+        
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+
+});
