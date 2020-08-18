bec6d1183bff4776c376ef1ee6011af9b85e141b
Kmarzi <k.l.marziali@gmail.com>
2020-08-18 15:19:24 -0400

    added newDivs

--- assets/script.js
+++ assets/script.js
@@ -30,27 +30,37 @@
 //         slide.video = new YT.Player(iframe)
 //     })
 // }
+// Disable the widescreen breakpoint
+
+
 $("#ingredients").on("click", function (event) {
     event.preventDefault();
     var delivery = $("#food").val();
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + delivery + "&apiKey=67db22d20b864a55b4e224c7af37816f&number=5";
+    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + delivery + "&apiKey=df8d7ec54f344b5fa40fa028efe7f6e3&number=10";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-    })
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://youtube-search1.p.rapidapi.com/katy-perry",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "youtube-search1.p.rapidapi.com",
-            "x-rapidapi-key": "1d694ca799mshfaf38605be0fb91p13aa9fjsn05eb2ac65e20"
+        for (var i = 0; i < response.length; i++) {
+            var recipe = $("<div>");
+            var title = $("<h2>")
+            title.text(response[i].title)
+            $(recipe).append(title);
+            $(".newDivs").append(recipe);
         }
-    }
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
+    })
+    // var settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://youtube-search1.p.rapidapi.com/" + delivery,
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "youtube-search1.p.rapidapi.com",
+    //         "x-rapidapi-key": "1d694ca799mshfaf38605be0fb91p13aa9fjsn05eb2ac65e20"
+    //     }
+    // }
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
 });
